04c2dbe662c9618f98f2b7326ad29400414735d9
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-08-26 19:35:13 -0300

    add test class

---
+++ src/test/java/com/softtek_preview/api_application/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softtek_preview.api_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
