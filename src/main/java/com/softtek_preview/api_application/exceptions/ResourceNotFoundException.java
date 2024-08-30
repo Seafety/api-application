e943ff9ef56323505aff96a87bcb275694205b59
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-08-30 00:36:02 -0300

    feat: create exception

---
+++ src/main/java/com/softtek_preview/api_application/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.softtek_preview.api_application.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
