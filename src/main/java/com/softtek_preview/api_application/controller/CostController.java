da5d5fb0707520c690e27f851fe72255842b2956
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-08-31 19:00:55 -0300

    feature: create get by id, create and get all costs

--- src/main/java/com/softtek_preview/api_application/controller/CostController.java
+++ src/main/java/com/softtek_preview/api_application/controller/CostController.java
@@ -1,9 +1,13 @@
 package com.softtek_preview.api_application.controller;
 
+import com.softtek_preview.api_application.domain.cost.CostRequestDTO;
+import com.softtek_preview.api_application.domain.cost.CostResponseDTO;
 import com.softtek_preview.api_application.service.CostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cost")
@@ -11,4 +15,22 @@ public class CostController {
 
     @Autowired
     private CostService costService;
+
+    @PostMapping
+    public ResponseEntity<CostResponseDTO> createCost(CostRequestDTO costRequestDTO){
+        CostResponseDTO cost = costService.createCost(costRequestDTO);
+        return ResponseEntity.ok(cost);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CostResponseDTO> getCostById(@PathVariable String id){
+        CostResponseDTO cost = costService.getCostById(id);
+        return ResponseEntity.ok(cost);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CostResponseDTO>> getAllCosts(){
+        List<CostResponseDTO> costs =  costService.getAllCosts();
+        return ResponseEntity.ok(costs);
+    }
 }
--- src/main/java/com/softtek_preview/api_application/service/CostService.java
+++ src/main/java/com/softtek_preview/api_application/service/CostService.java
@@ -1,7 +1,54 @@
 package com.softtek_preview.api_application.service;
 
+import com.softtek_preview.api_application.domain.cost.Cost;
+import com.softtek_preview.api_application.domain.cost.CostRequestDTO;
+import com.softtek_preview.api_application.domain.cost.CostResponseDTO;
+import com.softtek_preview.api_application.exceptions.ResourceNotFoundException;
+import com.softtek_preview.api_application.repositories.CostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CostService {
+
+    @Autowired
+    private CostRepository costRepository;
+
+    public CostResponseDTO createCost(CostRequestDTO costRequestDTO) {
+        Cost cost = convertToEntity(costRequestDTO);
+        Cost savedCost = costRepository.save(cost);
+        return null;
+    }
+
+    public CostResponseDTO getCostById(String id){
+        Cost cost = costRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cost not found"));
+        return convertToDTO(cost);
+    }
+
+    public List<CostResponseDTO> getAllCosts(){
+        return costRepository.findAll().stream().map(this::convertToDTO).collect(Collectors.toList());
+    }
+
+    private Cost convertToEntity(CostRequestDTO costRequestDTO) {
+        Cost cost = new Cost();
+        cost.setCategoria(costRequestDTO.categoria());
+        cost.setExercicio(costRequestDTO.exercicio());
+        cost.setPeriodo(costRequestDTO.exercicio());
+        cost.setValorAt(costRequestDTO.valorAt());
+        cost.setCodigoAt(costRequestDTO.codigoAt());
+        return cost;
+    }
+
+    private CostResponseDTO convertToDTO(Cost cost) {
+        return new CostResponseDTO(
+                cost.getExercicio(),
+                cost.getPeriodo(),
+                cost.getCodigoAt(),
+                cost.getCategoria(),
+                cost.getValorAt()
+        );
+    }
 }
