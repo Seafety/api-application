48baf198b8a16b73b3045b984fcf10737a852ecb
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-08-31 00:19:07 -0300

    feat: adding validations

--- src/main/java/com/softtek_preview/api_application/controller/ProjectController.java
+++ src/main/java/com/softtek_preview/api_application/controller/ProjectController.java
@@ -17,36 +17,30 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
-
-    // Create a new project
     @PostMapping
     public ResponseEntity<ProjectResponseDTO> createProject(@RequestBody ProjectRequestDTO projectRequestDTO) {
         ProjectResponseDTO project = projectService.createProject(projectRequestDTO);
         return ResponseEntity.ok(project);
     }
 
-    // Get a project by ID
     @GetMapping("/{id}")
     public ResponseEntity<ProjectResponseDTO> getProjectById(@PathVariable UUID id) {
         ProjectResponseDTO project = projectService.getProjectById(id);
         return ResponseEntity.ok(project);
     }
 
-    // Get all projects
     @GetMapping
     public ResponseEntity<List<ProjectResponseDTO>> getAllProjects() {
         List<ProjectResponseDTO> projects = projectService.getAllProjects();
         return ResponseEntity.ok(projects);
     }
 
-    // Update a project by ID
     @PutMapping("/{id}")
     public ResponseEntity<ProjectResponseDTO> updateProject(@PathVariable UUID id, @RequestBody ProjectRequestDTO projectRequestDTO) {
         ProjectResponseDTO project = projectService.updateProject(id, projectRequestDTO);
         return ResponseEntity.ok(project);
     }
 
-    // Delete a project by ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProject(@PathVariable UUID id) {
         projectService.deleteProject(id);
--- src/main/java/com/softtek_preview/api_application/domain/project/ProjectRequestDTO.java
+++ src/main/java/com/softtek_preview/api_application/domain/project/ProjectRequestDTO.java
@@ -3,24 +3,59 @@ package com.softtek_preview.api_application.domain.project;
 
 import com.softtek_preview.api_application.domain.custoVenda.CustoVendaDTO;
 import com.softtek_preview.api_application.domain.piramideVenda.PiramideVendaDTO;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+
+
 public record ProjectRequestDTO(
+        @NotNull(message = "O nome do projeto é obrigatório")
+        @Size(min = 1, max = 255, message = "O nome do projeto deve ter entre 1 e 255 caracteres")
         String projeto,
+
+        @NotNull(message = "A descrição é obrigatória")
+        @Size(min = 1, max = 255, message = "A descrição deve ter entre 1 e 255 caracteres")
         String descricao,
+
+        @NotNull(message = "O tipo de contrato é obrigatório")
         String tipoContrato,
+
+        @NotNull(message = "O módulo é obrigatório")
         List<String> modulo,
+
+        @NotNull(message = "O tipo de demanda é obrigatório")
         List<String> tipoDemanda,
+
+        @NotNull(message = "O baseline é obrigatório")
         int baseline,
+
+        @NotNull(message = "O valor do contrato é obrigatório")
         BigDecimal valorContrato,
+
+        @NotNull(message = "A pirâmide de vendas é obrigatória")
         PiramideVendaDTO piramideVendas,
+
+        @NotNull(message = "O custo de venda é obrigatório")
         CustoVendaDTO custoVenda,
+
+        @NotNull(message = "As horas de gestão são obrigatórias")
         int horasGestao,
+
+        @NotNull(message = "O baseline consumido é obrigatório")
         int baselineConsumido,
+
+        @NotNull(message = "A data de início do contrato é obrigatória")
         LocalDate inicioContrato,
+
+        @NotNull(message = "A data de fim do contrato é obrigatória")
         LocalDate fimContrato,
+
+        @NotNull(message = "O status é obrigatório")
         String status,
+
+        @NotNull(message = "O campo ativo é obrigatório")
         boolean ativo
 ) { }
