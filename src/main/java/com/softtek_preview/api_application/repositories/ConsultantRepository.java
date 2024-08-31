45a5e2094bea1e08e4d8e5f08ab4a95c73a278e7
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-08-31 19:00:14 -0300

    FIX: fix Project owners set

--- src/main/java/com/softtek_preview/api_application/repositories/ConsultantRepository.java
+++ src/main/java/com/softtek_preview/api_application/repositories/ConsultantRepository.java
@@ -5,9 +5,11 @@ import com.softtek_preview.api_application.domain.consultant.Consultant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface ConsultantRepository extends JpaRepository<Consultant, UUID> {
-
+    List<Consultant> findByIdIn(Set<UUID> ids);
 }
